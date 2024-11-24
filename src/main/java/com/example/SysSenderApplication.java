88b5f1186eef6f1336741c0040c5123211454805
Jayesh2026 <jayesh.savle@bnt-soft.com>
2024-11-25 00:21:21 +0530

    implement CommandLineRunner

--- src/main/java/com/example/SysSenderApplication.java
+++ src/main/java/com/example/SysSenderApplication.java
@@ -1,30 +1,29 @@
 package com.example;
 
-// import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import com.example.service.SyslogSenderService;
+import com.example.service.SyslogSenderService;
 
 @SpringBootApplication
-public class SysSenderApplication //implements CommandLineRunner 
-{
+public class SysSenderApplication implements CommandLineRunner {
 
-	// private final SyslogSenderService syslogSenderService;
+	private final SyslogSenderService syslogSenderService;
 
 
-	// public SysSenderApplication(SyslogSenderService syslogSenderService) {
-    //     this.syslogSenderService = syslogSenderService;
-    // }
+	public SysSenderApplication(SyslogSenderService syslogSenderService) {
+        this.syslogSenderService = syslogSenderService;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SysSenderApplication.class, args);
 	}
 
 	
-    // @Override
-    // public void run(String... args) throws Exception {
-    //     // Send a test syslog message on startup
-    //     syslogSenderService.sendTestSyslogMessage();
-    // }
+    @Override
+    public void run(String... args) throws Exception {
+        // Send a test syslog message on startup
+        syslogSenderService.sendTestSyslogMessage();
+    }
 }
